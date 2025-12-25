afde846fe129f4cc8cd4f71651664a67cfcbb582
Mr. HeFung <qq2236639958@outlook.com>
2025-12-25 18:53:10 +0800

    Update win-create-user.ps1

--- .github/workflows/scripts/win-create-user.ps1
+++ .github/workflows/scripts/win-create-user.ps1
@@ -13,8 +13,8 @@ $rawPassword += $charSet.Number | Get-Random -Count 4
 $rawPassword += $charSet.Special | Get-Random -Count 4
 $password = -join ($rawPassword | Sort-Object { Get-Random })
 $securePass = ConvertTo-SecureString $password -AsPlainText -Force
-New-LocalUser -Name "vum" -Password $securePass -AccountNeverExpires
-Add-LocalGroupMember -Group "Administrators" -Member "vum"
-Add-LocalGroupMember -Group "Remote Desktop Users" -Member "vum"
-echo "RDP_CREDS=User: vum | Password: $password" >> $env:GITHUB_ENV
-if (-not (Get-LocalUser -Name "vum")) { throw "User creation failed" }
+New-LocalUser -Name "RDP" -Password $securePass -AccountNeverExpires
+Add-LocalGroupMember -Group "Administrators" -Member "RDP"
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member "RDP"
+echo "RDP_CREDS=User: RDP | Password: $password" >> $env:GITHUB_ENV
+if (-not (Get-LocalUser -Name "RDP")) { throw "User creation failed" }
